e69cbfdc50bfe45ca532c4871aec9d0c5975875e
Denis Gorelov <godenl@mail.ru>
2023-02-16 22:21:49 +0700

    added tasks01,03,05

---
+++ Task01/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая 
+// 1. на вход принимает два числа и 
+// 2. проверяет, является ли первое число квадратом второго.
+
+// a = 25, b = 5 -> да
+// a = 2, b = 10 -> нет
+// a = 9, b = -3 -> да
+// a = -3 b = 9 -> нет
+
+Console.WriteLine("Введите число 1: ");
+Console.WriteLine("Введите число 2: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+int square = number2 * number2;
+if (number1 == square)
+    Console.WriteLine($"Введенное число {number1} являестя квадратом {number2}");
+else
+    Console.WriteLine($"Введенное число {number1} НЕ являестя квадратом {number2}");
---
+++ Task03/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая будет выдавать
+// название дня недели по заданному номеру.
+// 3 -> Среда
+// 5 -> Пятница
+
+Console.WriteLine("Введите цифру от 1 до 7, соотвтетсвующую дню недели");
+string number = Console.ReadLine();
+if (number == "1")
+    Console.WriteLine("это понедельник");
+if (number == "2")
+    Console.WriteLine("это вторник");
+if (number == "3")
+    Console.WriteLine("это среда");
+if (number == "4")
+    Console.WriteLine("это четверг");
+if (number == "5")
+    Console.WriteLine("это пятница");
+if (number == "6")
+    Console.WriteLine("это суббота");
+if (number == "7")
+    Console.WriteLine("это воскресенье");
+
+// можно использовать конструкцию if - else
+// это 1) упростит программу и не бует проходить по всем строкам
+// 2) можно задать любое значение, шире диапозона 1-7
+// пример: 
+// if (number == 1) Console.WriteLine("это понедельник");
+// else if (number == 2) Console.WriteLine("это вторник");
+// и т.д.
