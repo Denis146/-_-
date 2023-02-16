efe94b9bb544c46eccf099b706a3ad25fbac27db
Denis Gorelov <godenl@mail.ru>
2023-02-16 22:41:42 +0700

    added task07

---
+++ Task07/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход
+// трёхзначное число и на выходе показывает последнюю цифру
+// этого числа.
+// 456 -> 6
+// 782 -> 2
+// 918 -> 8
+Console.Write("введите трехзначное число  ");
+int namber = Convert.ToInt32(Console.ReadLine());
+if (namber >= 100 && namber <= 999)
+{
+    int lastnamber = namber % 10;
+Console.Write($"последняя цифра числа --> {lastnamber}");
+}
+else Console.Write("это не трехзначное число");
