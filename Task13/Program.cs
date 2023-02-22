8ee62ea9a6a113971bbb0fd80fb18cc0cbb4270b
Denis Gorelov <godenl@mail.ru>
2023-02-22 23:16:44 +0700

    added task13 HW

---
+++ Task13/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая выводит третью цивру заданного числа
+// или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.Write("введите число  ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+while (number > 999)
+number = number / 10;
+
+if (number >= 100 && number <= 999)
+{
+    int newNumber = ThirdDigit(number);
+    Console.WriteLine($" -> {newNumber}");
+}
+else Console.Write("третей цифры нет");
+
+
+int ThirdDigit(int num)
+{
+    return num % 10;
+}
