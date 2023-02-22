10ebc3587ccc3ae293a04e85027d9b86ff38096c
Denis Gorelov <godenl@mail.ru>
2023-02-22 21:05:06 +0700

    added tasks12,14

---
+++ Task12/Program.cs
@@ -0,0 +1,21 @@
+﻿//12. Напишите программу, которая будет 
+// 1) принимать на вход два числа и 
+// 2) выводить, является ли первое число кратным второму.
+// Если число 1 не кратно числу 2, то программа выводит остаток от деления.
+// 34, 5 -> не кратно, остаток 4
+// 16, 4 -> кратно
+
+Console.WriteLine("введите любые два числа");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+int result = Multiplicity(number1, number2);
+
+if (result == 0) Console.WriteLine($"число -> {number1} кратно {number2}");
+else Console.WriteLine($"число -> {number1} НЕ кратно {number2} , остаток от деления " + result);
+
+
+int Multiplicity(int num1, int num2)
+{
+    return num1 % num2;
+}
---
+++ Task14/Program.cs
@@ -0,0 +1,19 @@
+﻿// 14. Напишите программу, которая 
+// 1)принимает на вход число и 
+// 2)проверяет, кратно ли оно одновременно 7 и23
+// 14 -> нет
+// 46 -> нет
+// 161 -> да
+// т.к. в ответе д.б. да или нет, будем использовать bool метод
+
+
+Console.WriteLine("введите любые целое число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (MultiplicityTwoNumbers (number)) Console.WriteLine("-> да");
+else Console.WriteLine("-> нет");
+
+bool MultiplicityTwoNumbers (int num)
+{
+     return num % 7 == 0 && num % 23 == 0;
+}
