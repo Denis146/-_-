d25d93c2379b9c4618f09ef8c15eed61d6d1a032
Denis Gorelov <godenl@mail.ru>
2023-02-26 14:34:32 +0700

    added task22v2

---
+++ Task22/Program.cs
@@ -0,0 +1,21 @@
+﻿// 22. Напишите программу, которая
+// принимает на вход число N 
+// и выдает таблицу квадратов чисел от 1 до N 
+// 5 -> 1, 4, 9, 16, 25
+// 2 -> 1, 4
+
+
+Console.WriteLine("введите любое число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+Square(N);
+
+
+void Square(int num)
+{
+    int index = 1;
+    while (index <= num)
+    {
+        Console.WriteLine($"{index, 5}  -  {index * index}, 5"); // 5 в скобках выравнивает формат вывода чтобы цифры стояли ровно
+        index++;
+    }
+}
---
+++ Task22v2/Program.cs
@@ -0,0 +1,28 @@
+﻿// 22. второй вариант задачи. Через цикл FOR
+//  Напишите программу, которая
+// принимает на вход число N 
+// и выдает таблицу квадратов чисел от 1 до N 
+// 5 -> 1, 4, 9, 16, 25
+// 2 -> 1, 4
+
+        // метод ввода
+        int number = Prompt("введите натуральное число: ");
+
+        if (number > 0) PrintSqr(number);                       // проверка числа на натуральность
+        else Console.WriteLine("введено не натуральное число");
+
+        void PrintSqr(int num)                                  // не возвратный метод, вывод результата в консоль
+        {
+            Console.WriteLine("число    квадрат числа");
+            for (int i = 1; i <= num; i++)
+            {
+                Console.WriteLine($"{i,5}  {i * i,5}");
+            }
+        }
+
+        int Prompt(string message)                             // метод ввода
+        {
+            Console.WriteLine(message);
+            int cc = Convert.ToInt32(Console.ReadLine());
+            return cc;
+        }
