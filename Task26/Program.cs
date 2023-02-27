f75ac3dc1f508a677c7580530d965dbf01e2ff9f
Denis Gorelov <godenl@mail.ru>
2023-02-27 20:39:49 +0700

    added task26

---
+++ Task26/Program.cs
@@ -0,0 +1,23 @@
+﻿// 26. Напишите программу, которая
+// принимает на вход число и 
+// выдает количество цифр в чиселе
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5 
+
+Console.WriteLine("Введите целое, положительное число:  ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int digit = CheckDigit(number);
+Console.WriteLine($"в виденном числе {digit} знаков");
+
+int CheckDigit(int num)
+{
+    int result = 0;
+    while (num != 0)
+    {
+        num = num / 10;
+        result = result + 1;
+    }
+    return result;
+}
