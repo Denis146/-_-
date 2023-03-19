41bae1a54e6f37884440bffc514bcac15f6ab325
Denis Gorelov <godenl@mail.ru>
2023-03-19 08:39:59 +0700

    added tasks

---
+++ Task58/Program.cs
@@ -0,0 +1,65 @@
+﻿// Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+// 15 18
+
+int[,] CreateMatrixRndInt(int rows, int colums, int min, int max)
+{
+    int[,] matrix = new int[rows, colums];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],5}");
+        }
+        Console.WriteLine();
+    }
+}
+
+void MultiplyMatrix(int[,] firstMartrix, int[,] secomdMartrix, int[,] newMatrix)
+{
+  
+  for (int i = 0; i < newMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < newMatrix.GetLength(1); j++)
+        {
+            int sum = 0;
+            for (int k = 0; k < firstMartrix.GetLength(1); k++)
+            {
+                sum += firstMartrix[i, k] * secomdMartrix[k, j];
+            }
+            newMatrix[i, j] = sum;
+        }
+    }
+}
+
+
+int[,] array2d = CreateMatrixRndInt(4, 3, 1, 4);
+PrintMatrix(array2d);
+Console.WriteLine();
+
+int[,] secondArray2d = CreateMatrixRndInt(3, 4, 1, 5);
+PrintMatrix(secondArray2d);
+
+int[,] newMatrix = new int[4, 4];
+
+MultiplyMatrix(array2d, secondArray2d, newMatrix);
+Console.WriteLine($"Произведение первой и второй матриц:");
+PrintMatrix(newMatrix);
--- Черновик/Program.cs
+++ Черновик/Program.cs
@@ -1,23 +1,62 @@
-﻿Random random = new Random();
-int[,] arr = new int[random.Next(1, 10), random.Next(1, 10)];
-for (int i = 0; i < arr.GetLength(0); i++)
+﻿int[,] CreateMatrix (int row, int col, int min, int max) 
 {
-    for (int j = 0; j < arr.GetLength(1); j++)
+    int[,] matrix = new int[row, col];
+    Random rnd = new Random();   
+    for (int i = 0; i < matrix.GetLength(0); i++) 
     {
-        arr[i, j] = random.Next(1, 10);
-        Console.Write(arr[i, j] + " ");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i,j] = rnd.Next(min, max +1);
+        }
     }
-    Console.WriteLine();
-}
-Console.WriteLine("---------------------------");
-Console.WriteLine(arr.GetLength(0));
-for (int j = 0; j < arr.GetLength(1); j++)
+    return matrix;
+} 
+
+void PrintMatrix(int[,] matrix)
 {
-    double sum = 0;
-    for (int i = 0; i < arr.GetLength(0); i++)
+    for (int i = 0; i <  matrix.GetLength(0); i++)
     {
-        sum += arr[i, j];
-    }
-    Console.Write($"{ sum / arr.GetLength(0)} ");
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++) 
+        {
+            if (j < matrix.GetLength(1) -1) Console.Write($"{matrix[i, j], 1}|");
+            else Console.Write($"{matrix[i, j], 1}");
+        }
+        Console.WriteLine("|");        
+    }        
 }
-Console.ReadLine();
+
+static void SumStringMatrix(int[,] matrix)
+{
+    int index = 0, minsum = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            sum += matrix[i, j];
+        }
+        Console.WriteLine($"Сумма {i + 1} строки = {sum}");
+        if (i == 0)
+        {
+            minsum = sum;
+        }
+        else if (sum < minsum)
+        {
+            minsum = sum;
+            index = i;
+        }
+    }
+    string line = string.Empty;
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        line += matrix[index, j] + " ";
+    }
+    Console.WriteLine($"Строка с минимальной суммой элементов равна {line}. ");
+}
+
+   
+
+int[,] array2D = CreateMatrix(3, 4, 0, 9);
+PrintMatrix(array2D);
+SumStringMatrix(array2D);
